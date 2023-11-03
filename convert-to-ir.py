e6324a33f9283e9e749f3a9b3fcdd8eb04064a5f
Ojas D. Sawant <ojjsaw@gmail.com>
2023-11-03 04:36:29 +0000

    fixed size export ir

--- convert-to-ir.py
+++ convert-to-ir.py
@@ -3,4 +3,5 @@ from ultralytics import YOLO
 det_model = YOLO("models/original.pt")
 
 # generates onnx and then auto-converts to openvino IR
-det_model.export(format="openvino", dynamic=True, half=False)
+# https://docs.ultralytics.com/modes/export/#arguments
+det_model.export(format="openvino", imgsz=(416,416), half=False)
